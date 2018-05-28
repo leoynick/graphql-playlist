c42fa2516d3b66c0552bd43f4c616ac479665975
Yannick <leoynick@gmail.com>
2018-05-28 16:37:16 +0200

    Add author for detail

--- client/src/components/BookDetail.js
+++ client/src/components/BookDetail.js
@@ -4,14 +4,22 @@ import { graphql } from 'react-apollo';
 import { getBookQuery } from '../queries/queries'
 
 export default graphql(getBookQuery, {
-    options: props => { return { variables: { id: props.bookId }}}
-})( props  => (
+    options: props => ({ variables: { id: props.bookId }})
+})(( { data: { book }} , ...props )  => (
     <div>
-        <h2>This is the Detail</h2>
-        <div>
-            {props.data.book
-            ? <h3>{props.data.book && props.data.book.name}</h3>
-            : <div>Nada...</div>}
-        </div>
+        {book &&
+            <div>
+                <h2>{book && book.name}</h2>
+                <p>{book.genre}</p>
+                <div>
+                    <h3>{book.author.name}</h3>
+                    <span>All books by this Author</span>
+                    <ul>
+                        {book.author.books.map(b => (
+                            <li key={b.id}>{b.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>}
     </div>
 ))
